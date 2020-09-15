dba4b5e3dc40d7b6b3d339b1a58a891e97faa099
hasalioma2 <56221788+hasalioma2@users.noreply.github.com>
2020-09-15 19:35:30 +0300

    Add project files.

---
+++ WebApi/models/DCandidate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.models
+{
+    public class DCandidate
+    {
+        [Key]
+        public int id { get; set; }
+        [Column(TypeName ="nvarchar(100)")]
+        public string fullName { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string mobile { get; set; }
+        public string email { get; set; }
+        public int age { get; set; }
+        public string bloodGroup { get; set; }
+        public string address { get; set; }      
+
+    }
+}
